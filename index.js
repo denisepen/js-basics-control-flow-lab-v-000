a3ca4da83fb412836c745880fb8ee5df8b79517b
Denise Pendleton <dpendl1@hotmail.com>
2018-05-22 14:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Write your code in this file!
-let message
+let message;
 
 function scuberGreetingForFeet(distance) {
-    
+
     if(distance <= 400) {
-      message = 'This one is on me!'
+      message = 'This one is on me!';
     } else if (distance >= 2000 && distance <= 2500) {
-      message = 'I will gladly take your thirty bucks.'
+      message = 'I will gladly take your thirty bucks.';
     }
     else {
-      message = 'No can do.'
+      message = 'No can do.';
     }
 
 }
