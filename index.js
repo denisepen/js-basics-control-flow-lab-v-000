3da8a2c740ed8bf8a22663c4c5989b221ef2b027
Denise Pendleton <dpendl1@hotmail.com>
2018-05-22 14:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ function scuberGreetingForFeet(distance) {
     return message
 }
 
-const ternaryCheckCity = (destination === "NYC") ? "Ok, sounds good." : "No go.";
+function ternaryCheckCity = (destination === "NYC") ? "Ok, sounds good." : "No go.";
